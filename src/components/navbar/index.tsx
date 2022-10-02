431bcb97fce0d29d38e6d51a9c1d7fe7abc92f0f
Vany Buava <viniciusbuavaleite@gmail.com>
2022-10-02 13:14:09 -0300

    navbar responsiva adicionada

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,16 +1,22 @@
-import React from "react";
-import { Conteiner, ConteinerLink, LinkTo, ListaLink, Logo, NavBar } from "./styled";
+import React, { useState } from "react";
+import { Conteiner, ConteinerLink, Linha1, Linha2, Linha3, LinkTo, LinkToCarrinho, ListaLink, Logo, MenuHamburg, NavBar } from "./styled";
 
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    const [ativo, setAtivo] = useState(false)
+    return (
         <Conteiner>
             <Logo>BL Esculturas</Logo>
-            <NavBar>
-                <ListaLink>
+            <MenuHamburg onClick={() => setAtivo(!ativo)} ativo={ativo}>
+                <Linha1 ativo={ativo}></Linha1>
+                <Linha2 ativo={ativo}></Linha2>
+                <Linha3 ativo={ativo}></Linha3>
+            </MenuHamburg>
+            <NavBar ativo={ativo}>
+                <ListaLink ativo={ativo}>
                     <ConteinerLink><LinkTo to="/">Home</LinkTo></ConteinerLink>
                     <ConteinerLink><LinkTo to="/produtos">Produtos</LinkTo></ConteinerLink>
                 </ListaLink>
-                <LinkTo to="/carrinho">Meu carrinho</LinkTo>
+                    <LinkToCarrinho ativo={ativo} to="/carrinho">Meu carrinho</LinkToCarrinho>
             </NavBar>
         </Conteiner>
     )
--- src/components/navbar/styled.tsx
+++ src/components/navbar/styled.tsx
@@ -1,5 +1,6 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Link } from 'react-router-dom'
+import { colors } from '../../theme/color'
 
 export const Conteiner = styled.header`
     color: white;
@@ -14,19 +15,90 @@ export const Logo = styled.h1`
     font-size: 2rem;
     white-space: nowrap;
 `
-
-export const NavBar = styled.nav`
+interface Props {
+    ativo: boolean
+}
+export const NavBar = styled.nav<Props>`
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-around;
     width: 100%;
-    `
-export const ListaLink = styled.ul`
+    @media screen and (max-width: 880px){
+        ${props => props.ativo === false && css`
+            display: none;
+        `}
+        ${props => props.ativo === true && css`
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100vh;
+            z-index: 2;
+            background-color: ${colors.backgroundCard};
+            flex-direction: column;
+        `}
+    }
+`
+
+export const MenuHamburg = styled.button<Props>`
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: transparent;
+    border: 0;
+    width: 3.5rem;
+    height: 2rem;
+    display: none;
+    z-index: 3;
+    @media screen and (max-width: 880px){
+        display: flex;
+    }
+`
+
+export const Linha1 = styled.div<Props>`
+    width: 100%;
+    height: .3rem;
+    background-color: #ffffff;
+    transition: 1s;
+    ${props => props.ativo === true && css`
+        transform: rotate(30deg) translate(15%, 130%);
+        transition: 1s;
+    `}
+`
+export const Linha2 = styled.div<Props>`
+    width: 100%;
+    height: .3rem;
+    background-color: #ffffff;
+    transition: 1s;
+    ${props => props.ativo === true && css`
+        margin-left: 5rem;
+        opacity: 0;
+        transition: 1s;
+    `}
+`
+export const Linha3 = styled.div<Props>`
+    width: 100%;
+    height: .3rem;
+    background-color: #ffffff;
+    transition: 1s;
+    ${props => props.ativo === true && css`
+        transform: rotate(145deg) translate(-25%, 250%);
+        transition: 1s;
+    `}
+`
+
+
+export const ListaLink = styled.ul<Props>`
     display: flex;
     justify-content: space-evenly;
     align-items: center;
     list-style: none;
     width: 70%;
+    @media screen and (max-width: 880px){
+        ${props => props.ativo === true && css`
+            flex-direction: column;
+            height: 80vh;
+        `}
+    }
+
 `
 export const ConteinerLink = styled.li``
 export const LinkTo = styled(Link)`
@@ -36,5 +108,15 @@ export const LinkTo = styled(Link)`
     font-weight: 500;
     &:hover{
         cursor: url(${require('../../assets/cursor/cursorAtivo.png')}), auto;
+    }
+`
+
+export const LinkToCarrinho = styled(LinkTo)<Props>`
+    
+    @media screen and (max-width: 880px){
+        ${props => props.ativo === true && css`
+            height: 70vh;
+            padding-left: 3rem;
+        `}
     }
 `
